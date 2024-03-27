1ca48e59e0a82332f66591f9b747b830f0a4ff09
scott <scottw91@live.ca>
2024-03-27 01:39:15 -0400

    server rdy to route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -12,3 +13,19 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, './path/to/static/assets')));
 
 app.use('/api', require('./apiRoutes'));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './path/to/index.html'))
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+app.listen(port, function () {
+  console.log("Knock, knock");
+  console.log("Who's there?");
+  console.log(`Your server, listening on port ${port}`);
+});
